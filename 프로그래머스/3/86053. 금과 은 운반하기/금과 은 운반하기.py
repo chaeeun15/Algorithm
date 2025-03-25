260565a1203c78c3f301895d6d1b30933d8a3342
chaeeun15 <68808642+chaeeun15@users.noreply.github.com>
2025-03-25 20:42:20 +0900

    [level 3] Title: 금과 은 운반하기, Time: 6485.09 ms, Memory: 21.5 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.py
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.py
@@ -16,10 +16,12 @@ def solution(a, b, g, s, w, t):
             gold += min(g[i], move_cnt * w[i])
             silver += min(s[i], move_cnt * w[i])
             total += min(g[i] + s[i], move_cnt * w[i])
-        # 조건을 만족하는 경우(시간이 충분한 경우) -> 시간을 줄임
+        # 조건을 만족하는 경우(시간이 충분한 경우) -> 더 적은 시간이 가능할 수도 있으므로 시간을 줄여봄
+        # 현재 mid값이 포함되도록 end = mid로 변경
         if gold >= a and silver >= b and total >= a+b:
             end = mid
         # 조건 만족하지 못하는 경우 -> 시간을 늘림
+        # 현재 mid 값이 포함될 필요 없음
         else:
             start = mid + 1
     return start
