db0717455993d741394cdc01218c3cdd001afb58
chaeeun15 <68808642+chaeeun15@users.noreply.github.com>
2025-03-25 20:38:33 +0900

    [level 3] Title: 금과 은 운반하기, Time: 5804.64 ms, Memory: 21.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.py
@@ -0,0 +1,25 @@
+def solution(a, b, g, s, w, t):
+    # 특정 시간일 때 조건을 만족할 수 있는지 이분탐색으로 검사
+    # 최악의 경우: ((2*10^9) * (2*10^5))/1
+    start = 0
+    end = 4*10**14
+    while start < end:
+        mid = (start + end) // 2
+        gold = silver = total = 0
+        
+        for i in range(len(t)):
+            # 운반 횟수는 총 걸린 시간을 한 번 운반할 때 걸린 시간으로 나눈 값
+            move_cnt = mid // (2*t[i])
+            # 처음 운반은 편도로 가능
+            if mid%(2*t[i]) >= t[i]:
+                move_cnt += 1
+            gold += min(g[i], move_cnt * w[i])
+            silver += min(s[i], move_cnt * w[i])
+            total += min(g[i] + s[i], move_cnt * w[i])
+        # 조건을 만족하는 경우(시간이 충분한 경우) -> 시간을 줄임
+        if gold >= a and silver >= b and total >= a+b:
+            end = mid
+        # 조건 만족하지 못하는 경우 -> 시간을 늘림
+        else:
+            start = mid + 1
+    return start
