5a379c6858066c1b611d277dc0fad55ed3956a23
chaeeun15 <68808642+chaeeun15@users.noreply.github.com>
2025-03-29 14:53:35 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,12 @@
+from collections import deque
+def solution(cards1, cards2, goal):
+    queue1 = deque(cards1)
+    queue2 = deque(cards2)
+    for word in goal:
+        if queue1 and word == queue1[0]:
+            queue1.popleft()
+        elif queue2 and word == queue2[0]:
+            queue2.popleft()
+        else:
+            return "No"
+    return "Yes"
