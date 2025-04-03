1e45af70e881c6c52bdd068e49cb090e48c2ef3a
chaeeun15 <68808642+chaeeun15@users.noreply.github.com>
2025-04-03 14:44:03 +0900

    [level 1] Title: 햄버거 만들기, Time: 148.48 ms, Memory: 25.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,10 @@
+def solution(ingredient):
+    answer = 0
+    hamburger = [1, 2, 3, 1]
+    stack = []
+    for i in ingredient:
+        stack.append(i)
+        if stack[-4:] == hamburger:
+            del stack[-4:]
+            answer += 1
+    return answer
