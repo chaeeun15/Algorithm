f2f1bd9f850a265d08a0e89d46a6cc14d28ce8f2
chaeeun15 <68808642+chaeeun15@users.noreply.github.com>
2025-04-08 13:43:29 +0900

    [level 2] Title: 전화번호 목록, Time: 106.30 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -5,7 +5,6 @@ def solution(phone_book):
         i = 1
         len_num = len(number)
         while idx+i < book_len and phone_book[idx+i][:len_num] == number:
-            # if phone_book[idx+i][:len_num] == number:
             return False
             i += 1
     return True
