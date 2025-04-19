dd1a238fdbbe8928831f6bd06b2fb65f4a3132c2
chaeeun15 <68808642+chaeeun15@users.noreply.github.com>
2025-04-19 14:24:58 +0900

    [level 2] Title: 카펫, Time: 0.15 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,11 +1,11 @@
 def solution(brown, yellow):
-    # 24->24*1 / 12*2 / 8*3 / 6*4
     # yellow로 만들 수 있는 경우의 수 tuple로 저장
     yellow_grid = [(yellow, 1)]
     for col in range(2, int(yellow**(1/2))+1):
         if yellow % col == 0:
             yellow_grid.append((yellow//col, col))
-    print(yellow_grid)
+    
+    # yellow의 테두리만큼의 영역이 brown값이랑 같으면 리턴
     for row, col in yellow_grid:
         if (row+2)*2 + col*2 == brown:
             return [row+2, col+2]
