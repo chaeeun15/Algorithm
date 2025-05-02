fc641cbf41ad73795f76a1c8853eae6f8c911277
chaeeun15 <68808642+chaeeun15@users.noreply.github.com>
2025-05-02 15:59:42 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,31 +1,27 @@
 import sys
-
 input = sys.stdin.readline
-N = int(input())
-num = list(map(int, input().split()))
-op = list(map(int, input().split()))  # +, -, *, //
-
-maximum = -1e9
-minimum = 1e9
-
+N = int(input()) # 숫자의 개수
+numbers = list(map(int, input().split()))
+operations = list(map(int, input().split()))
+min_num = int(1e9)
+max_num = int(-1e9)
 
 def dfs(depth, total, plus, minus, multiply, divide):
-    global maximum, minimum
+    global max_num, min_num
+    # base case
     if depth == N:
-        maximum = max(total, maximum)
-        minimum = min(total, minimum)
+        max_num = max(max_num, total)
+        min_num = min(min_num, total)
         return
-
     if plus:
-        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+        dfs(depth+1, total + numbers[depth], plus-1, minus, multiply, divide)
     if minus:
-        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
+        dfs(depth+1, total - numbers[depth], plus, minus-1, multiply, divide)
     if multiply:
-        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
+        dfs(depth+1, total * numbers[depth], plus, minus, multiply-1, divide)
     if divide:
-        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
+        dfs(depth+1, int(total / numbers[depth]), plus, minus, multiply, divide - 1)
 
-
-dfs(1, num[0], op[0], op[1], op[2], op[3])
-print(maximum)
-print(minimum)
+dfs(1, numbers[0], operations[0], operations[1], operations[2], operations[3])
+print(max_num)
+print(min_num)
