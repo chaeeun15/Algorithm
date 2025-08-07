8664fe873954a54114c15aeaa88e73d8b1c08851
chaeeun15 <68808642+chaeeun15@users.noreply.github.com>
2025-08-07 20:44:13 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 704 ms, Memory: 55900 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                // x가 같다면 y를 기준으로 오름차순 정렬
+                // x, y 범위 오버플로우 생각할 필요 없음
+                return e1[1] - e2[1];
+            } else {
+                // x를 기준으로 오름차순 정렬
+                return e1[0] - e2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
