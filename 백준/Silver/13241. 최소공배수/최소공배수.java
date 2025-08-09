d0ac7cab10bb106f7e9e63257806584c993925af
chaeeun15 <68808642+chaeeun15@users.noreply.github.com>
2025-08-09 14:00:01 +0900

    [Silver V] Title: 최소공배수, Time: 184 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static long lcm(int a , int b) {
+        return ((long) a * b) / gcd(a, b);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(lcm(a, b));
+    }
+}
